0e9cb9f65a443e1dbd0d730e1c02628dfa610f36
kyuuss <adam.czogallik@pm.me>
2023-08-17 22:58:08 +0200

    feat: load ent variables

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT must be set")
+	}
+
+	fmt.Println("The port ist: ", port)
+}
