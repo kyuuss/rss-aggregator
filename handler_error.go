12be31f3524cfda16b4ea50d90b18ebd6626f0d6
kyuuss <adam.czogallik@pm.me>
2024-01-08 22:55:27 +0100

    fix: set correct error status code

--- handler_error.go
+++ handler_error.go
@@ -3,5 +3,5 @@ package main
 import "net/http"
 
 func handlerError(w http.ResponseWriter, request *http.Request) {
-	respondWithError(w, 200, "Something went wrong")
+	respondWithError(w, 400, "Something went wrong")
 }
