0856b50aecec0abfb373eddc5b3de8bdb376c2ed
kyuuss <adam.czogallik@pm.me>
2024-01-15 20:01:59 +0100

    feat: add migration stub

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,10 @@
+-- + goose Up
+CREATE TABEL users (
+    id UUID PRIMARY KEY
+    created_at TIMESPTAMP NOT NULL
+    updated_at TIMESPTAMP NOT NULL
+    name TEXT NOT NULL
+);
+
+-- +goose Down
+DROP TABLE users;
