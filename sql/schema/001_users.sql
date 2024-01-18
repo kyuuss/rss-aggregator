e7b55701abc8d7bc8052ccdc5f28ff0146928f2a
kyuuss <adam.czogallik@pm.me>
2024-01-18 21:45:47 +0100

    fix: add missing comma

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,8 +1,8 @@
 -- + goose Up
 CREATE TABEL users (
-    id UUID PRIMARY KEY
-    created_at TIMESPTAMP NOT NULL
-    updated_at TIMESPTAMP NOT NULL
+    id UUID PRIMARY KEY,
+    created_at TIMESPTAMP NOT NULL,
+    updated_at TIMESPTAMP NOT NULL,
     name TEXT NOT NULL
 );
 
